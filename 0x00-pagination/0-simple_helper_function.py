dab0468d8af767d7f08c195b1c9261c433fd531c
SemTheLegend <semmathe@gmail.com>
2023-04-18 14:22:00 +0000

    first two tasks

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module describes a function that takes two integer arguments.
+
+The function should return a tuple of size two containing a start index
+and an end index corresponing to the range of indexes to return in a list
+for those particular pagination parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """Returns a tuple of size two containing a start index and end index."""
+
+    return (page - 1) * page_size, page * page_size
